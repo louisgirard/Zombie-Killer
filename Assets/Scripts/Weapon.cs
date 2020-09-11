154fa6a88d70cdf3720508283a30670c164229aa
louisgirard <louis.girard17@hotmail.com>
2020-09-11 13:06:13 +0200

    add raycast

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range);
+        print(hit.transform.name);
+    }
+}
