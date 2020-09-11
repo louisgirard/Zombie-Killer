990db24df5b4666660de81baa128b2d0cb2d482f
louisgirard <louis.girard17@hotmail.com>
2020-09-11 13:51:12 +0200

    add shoot effect

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] int damage = 1;
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] ParticleSystem hitEffect;
 
     void Update()
     {
@@ -28,11 +29,18 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
         {
+            HitImpact(hit);
             EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
             if (enemy)
             {
                 enemy.loseHealth(damage);
             }
         }
+    }
+
+    private void HitImpact(RaycastHit hit)
+    {
+        ParticleSystem impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact.gameObject, hitEffect.main.duration);
     }
 }
