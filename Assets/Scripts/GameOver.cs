60fe2d6a1303203bf9ad2f05208b8f6ac9ee8cf2
louisgirard <louis.girard17@hotmail.com>
2020-09-15 15:24:36 +0200

    add game over UI

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+                Application.Quit();
+    #endif
+    }
+}
