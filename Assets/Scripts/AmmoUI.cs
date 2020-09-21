7dfa3d0a92a145d4622c95da2bfa8fbb9a50ea96
louisgirard <louis.girard17@hotmail.com>
2020-09-21 16:30:33 +0200

    add ammo ui

---
+++ Assets/Scripts/AmmoUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoUI : MonoBehaviour
+{
+    [SerializeField] AmmoType ammoType;
+    [SerializeField] Ammo ammoSlot;
+
+    Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = ammoSlot.GetAmmo(ammoType).ToString();
+    }
+}
