3e87c74baaf8c64d3c394eb88f84e6e44e693049
louisgirard <louis.girard17@hotmail.com>
2020-09-21 17:18:08 +0200

    add blood splash ui

---
+++ Assets/Scripts/DisplayDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayDamage : MonoBehaviour
+{
+    CanvasGroup canvasGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();   
+    }
+
+    public void show()
+    {
+        canvasGroup.alpha = 1;
+        StartCoroutine(BloodFade());
+    }
+
+    IEnumerator BloodFade()
+    {
+        while(canvasGroup.alpha >= 0)
+        {
+            canvasGroup.alpha -= .05f;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+}
--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -6,15 +6,18 @@ public class EnemyAttack : MonoBehaviour
 {
     [SerializeField] int damage = 5;
     PlayerHealth player;
+    DisplayDamage displayDamage;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerHealth>();
+        displayDamage = FindObjectOfType<DisplayDamage>();
     }
 
     public void AttackHitEvent()
     {
         player.LoseHealth(damage);
+        displayDamage.show();
     }
 }
