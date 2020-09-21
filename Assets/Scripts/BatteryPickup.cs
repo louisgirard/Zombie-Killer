0aaa7bdc8176dfeeeb165687d51a407a3b70e3ff
louisgirard <louis.girard17@hotmail.com>
2020-09-21 10:57:39 +0200

    add flashlight system

---
+++ Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            Flashlight flashlight = other.GetComponentInChildren<Flashlight>();
+            flashlight.ChargeBattery();
+            Destroy(gameObject);
+        }        
+    }
+}
---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    float maxIntensity = 10f;
+    Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        maxIntensity = light.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = light.intensity - lightDecay * Time.deltaTime;
+    }
+
+    public void ChargeBattery()
+    {
+        light.intensity = maxIntensity;
+    }
+}
